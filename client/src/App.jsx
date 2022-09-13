9fa055d2184b279971eff77d22e183f5203a068d
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-13 14:06:08 -0300

    start routes api rest

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Home } from "./pages/home/home"
+import { Form } from "./components/form/form"
+import { Footer } from "./components/footer/footer"
+import "./index.css"
+
+export function App() {
+  return (
+    <>
+      <Home />
+      <Form />
+      <Footer />
+    </>
+  );
+}
+
+
---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+
+export function Footer() {
+	return (
+		<>
+			<footer className="footer">
+				<a
+					class="text-decoration-none text-light"
+					target="_blank"
+					href="https://vanderleimiguel.herokuapp.com/"
+				>&copy; Copyright <strong>Vanderlei Miguel</strong></a
+				>
+			</footer>
+		</>
+	);
+}
