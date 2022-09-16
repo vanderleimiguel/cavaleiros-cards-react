2fa6067819b57ae03e2eb44832d2fc93775ea862
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-15 21:29:10 -0300

    styles header

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import './Header.css'
+import logo from '../../assets/logo.png'
 
 export function Header() {
   return (
     <div className='header'>
-      <h1>Cavaleiros do Zodiaco</h1>
+      <div className='row'>
+        <div className="Logo">
+          <img src={logo} width='70px' alt='Logo Cavaleiros do Zodiaco' className='Logo__icone' />
+          <span className='Logo__titulo'>Cavaleiros do Zodiaco</span>
+        </div>
+      </div>
     </div>
   )
 }
