892e250a11f91f1e00dd3cbc85145ce6dfdfcb8d
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-17 14:53:14 -0300

    update modal cards

--- vite-project/src/pages/Home/Home.jsx
+++ vite-project/src/pages/Home/Home.jsx
@@ -1,43 +1,44 @@
 import { Card } from '../../components/Card/Card'
 import { api } from '../../utils/Api/Api'
 import { useState, useEffect } from 'react'
+import Modal from "react-modal"
 
 import './Home.css'
-// import Modal from "react-modal"
 
-// const customStyles = {
-//   content: {
-//     top: "50%",
-//     left: "50%",
-//     right: "auto",
-//     bottom: "auto",
-//     marginRight: "-50%",
-//     width: "40rem",
-//     height: "50rem",
-//     transform: "translate(-50%, -50%)",
-//     backgroundColor: " rgba(0, 0, 0, 0.8)",
-//     borderRadius: "15px",
-//   },
-//   overlay: {
-//     background: "rgba(0, 0, 0, 0.4)",
-//   },
-// };
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    width: "30rem",
+    height: "30rem",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: " rgba(0, 0, 0, 0.8)",
+    borderRadius: "15px",
+  },
+  overlay: {
+    background: "rgba(0, 0, 0, 0.4)",
+  },
+};
 
-// Modal.setAppElement("#root");
+Modal.setAppElement("#root");
 
 export function Home() {
   const [cavaleiroList, setCavaleiroList] = useState([]);
-  // const [modalIsOpen, setModalIsOpen] = useState(false);
-  // const [uniqueAnime, setUniqueAnime] = useState({});
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [uniqueCavaleiro, setUniqueCavaleiro] = useState({});
+
 
   async function getCavaleiro() {
     const cavaleiros = await api.getAllCavaleiros();
     setCavaleiroList(cavaleiros);
   }
 
-  // function handleModal() {
-  //   setModalIsOpen(!modalIsOpen);
-  // }
+  function handleModal() {
+    setModalIsOpen(!modalIsOpen);
+  }
 
   useEffect(() => {
     getCavaleiro();
@@ -49,28 +50,28 @@ export function Home() {
       <div className="card-list">
         {cavaleiroList.map((item, index) => {
           return (
-            // <button
-            //   className="button-card"
-            //   onClick={() => {
-            //     setUniqueAnime(item);
-            //     handleModal();
-            //   }}
-            //   key={index}
-            // >
-            <Card
+            <button
+              className="button-card"
+              onClick={() => {
+                setUniqueCavaleiro(item);
+                handleModal();
+              }}
               key={index}
-              nome={item.nome}
-              constelacao={item.constelacao}
-              categoria={item.categoria}
-              tecnica={item.tecnica}
-              idade={item.idade}
-            />
-            // </button>
+            >
+              <Card
+                key={index}
+                nome={item.nome}
+                constelacao={item.constelacao}
+                categoria={item.categoria}
+                tecnica={item.tecnica}
+                idade={item.idade}
+              />
+            </button>
           );
         })}
       </div>
-      {
-      /* <Modal
+
+      <Modal
         isOpen={modalIsOpen}
         onRequestClose={handleModal}
         style={customStyles}
@@ -93,15 +94,16 @@ export function Home() {
               }}
               onClick={handleModal}
             >
-              <CgClose size={28} color="red" />
+              {/* <CgClose size={28} color="red" /> */}
             </button>
           </section>
-          <h2>{uniqueAnime.title}</h2>
-          <h3>{uniqueAnime.gender}</h3>
-          <h3>{uniqueAnime.protagonist}</h3>
-          <h3>{uniqueAnime.year}</h3>
+          <h2>{uniqueCavaleiro.nome}</h2>
+          <h3>{uniqueCavaleiro.constelacao}</h3>
+          <h3>{uniqueCavaleiro.categoria}</h3>
+          <h3>{uniqueCavaleiro.tecnica}</h3>
+          <h3>{uniqueCavaleiro.idade}</h3>
         </section>
-      </Modal> */}
+      </Modal>
 
     </div>
   );
